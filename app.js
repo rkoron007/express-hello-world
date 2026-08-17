d37cd4175b5f9065629036d32fb7f1ff018cc652
Rose Koron <rose@render.com>
2026-08-17 13:35:35 -0700

    Update favicon

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => res.type('html').send(html));
 app.get("/health", (req, res) => res.json({ status: "ok" }));
 app.get("/version", (req, res) => res.json({ version: "1.1.0" }));
 
-const server = app.listen(port, () => console.log(`My example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
